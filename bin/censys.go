635dbfffad5e40ed242f7d1ca1cee2c8063d4547
perfgao <perfgao@gmail.com>
2017-10-25 19:06:58 +0800

    update

--- bin/censys.go
+++ bin/censys.go
@@ -2,14 +2,25 @@ package main
 
 import (
     "perfgao/censys_io"
+    "perfgao/censys_io/config"
     "github.com/golang/glog"
     "flag"
 )
 
 
 func main () {
+    file := flag.String("c", "", "config file")
     flag.Parse()
     defer glog.Flush()
+
+    if *file == "" {
+        glog.Fatal("must \"-c\" appoint config file")
+    }
+
+    var conf config.Config
+    conf.Load(*file)
+
+    censys.Sqlinit(conf.Sql)
 
     var root censys.RootCert
 
